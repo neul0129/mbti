ffd59133a47284c59d97b0b24cb163098a0a2359
neul0129 <neul0129@naver.com>
2021-11-15 12:31:41 +0900

    header

--- src/header.js
+++ src/header.js
@@ -2,7 +2,7 @@ const header = () =>{
     return(
         <div className="header">
             <h1 className="logo">MBTI</h1>
-            <p className="login">ADMIN</p>
+            <button className="login">ADMIN</button>
         </div>
     );
 }
